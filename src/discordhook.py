5b43bbff1a81ddb0583732427f1b2810d988af15
Absolute (Ben .S) <absolute.intros@gmail.com>
2017-09-10 16:21:52 +0100

    Update discordhook.py

--- src/discordhook.py
+++ src/discordhook.py
@@ -2,24 +2,24 @@ import requests
 
 class Client:
 
-    def __init__(self, url):
+    def __init__(self, url, **kwargs):
         # URL to the Webhook, should be provided by Discord. (required)
         self.url = url
 
         # The message contents (up to 2000 characters), required if no embed.
-        self.message = None
+        self.message = kwargs.get('message')
         
         # Override the default username of the webhook, optional.
-        self.name = None
+        self.name = kwargs.get('name')
 
         # Override the default avatar of the webhook, optional.
-        self.avatar_url = None
+        self.avatar_url = kwargs.get('avatar_url')
 
         # True if this is a TTS message, optional.
-        self.tts = None
+        self.tts = kwargs.get('tts')
 
         # Embedded rich content, required if no message.
-        self.embed = None
+        self.embed = kwargs.get('embed')
 
     def send(self):
         
@@ -27,7 +27,7 @@ class Client:
                 'username': self.name,
                 'avatar_url': self.avatar_url,
                 'tts': self.tts,
-                'embeds': [self.embed]
+                'embeds': [self.embed] if self.embed else None
                 }
         r = requests.post(self.url, json=data)
         if r.status_code != 204:
